f22a1238c88f36da77b72f3bad4448c0782c850e
Hakan KARAYILMAZ <hkankyilmazz@gmail.com>
2023-07-14 01:00:44 +0300

    some method add to Menu class

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ class Menu {
     constructor(elem) {
         this._elem = elem;
         elem.onclick = this.onClick.bind(this);
+        elem.onmouseenter = this.onMouseEnter.bind(this);
+        elem.onmouseleave = this.onMouseLeave.bind(this);
     }
 
     showAlert(text) {
@@ -12,8 +14,22 @@ class Menu {
         let action = event.target.dataset.action;
         if (action) {
             this.showAlert(action);
+            document.querySelector()
         }
     };
+
+    onMouseEnter(event) {
+        let _type = event.target.tagName
+        if (_type = "LI") {
+            alert("Mouse hover")
+        }
+    }
+    onMouseLeave(event) {
+        let _type = event.target.tagName
+        if (_type = "LI") {
+            alert("Mouse leave")
+        }
+    }
 }
 
 
