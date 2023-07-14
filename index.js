cb89831f14046c2abc6f87a07ad4722a5ea68e57
Hakan KARAYILMAZ <99439329+hkankyilmaz@users.noreply.github.com>
2023-07-14 12:31:21 +0300

    update menu class

--- index.js
+++ index.js
@@ -1,38 +1,45 @@
 class Menu {
-    constructor(elem) {
-        this._elem = elem;
-        elem.onclick = this.onClick.bind(this);
-        elem.onmouseenter = this.onMouseEnter.bind(this);
-        elem.onmouseleave = this.onMouseLeave.bind(this);
-    }
+  /**
+   *
+   * @param {String}  CSS Selector
+   * @param {String}  CSS Selector
+   */
+  constructor(s, c) {
+    this.s = document.querySelector(s);
+    document.querySelector(s).onclick = this.onClick.bind(this);
+    document.querySelectorAll(c).forEach((i) => {
+      i.onmouseenter = this.onMouseEnter.bind(this);
+    });
+    document.querySelectorAll(c).forEach((i) => {
+      i.onmouseleave = this.onMouseLeave.bind(this);
+    });
+  }
 
-    showAlert(text) {
-        alert(text)
-    }
+  showAlert(text) {
+    alert(text);
+  }
 
-    onClick(event) {
-        let action = event.target.dataset.action;
-        if (action) {
-            this.showAlert(action);
-            document.querySelector()
-        }
-    };
+  onClick(event) {
+    let action = event.target.dataset.action;
+    if (action) {
+      this.showAlert(action);
+      document.querySelector();
+    }
+  }
 
-    onMouseEnter(event) {
-        let _type = event.target.tagName
-        if (_type = "LI") {
-            alert("Mouse hover")
-        }
+  onMouseEnter(event) {
+    let _type = event.target.tagName;
+    console.log(_type);
+    if (_type == "LI") {
+      alert("Mouse hover");
     }
-    onMouseLeave(event) {
-        let _type = event.target.tagName
-        if (_type = "LI") {
-            alert("Mouse leave")
-        }
+  }
+  onMouseLeave(event) {
+    let _type = event.target.tagName;
+    if (_type == "LI") {
+      alert("Mouse leave");
     }
+  }
 }
 
-
-var menu = document.querySelector("nav");
-new Menu(menu);
-
+new Menu("nav", "li");
